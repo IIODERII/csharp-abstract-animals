772d241b414c3bb7df021e095663b9741b64ae04
IIODERII <oderrisi@gmail.com>
2024-04-24 14:11:32 +0200

    exercise completed

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzzzz");
+        }
+
+        public abstract void Verso();
+
+        public abstract void Mangia();
+    }
+
+    public class Mucca : Animale {
+        public override void Verso()
+        {
+            Console.WriteLine("Mucca *** **** MUUUUUUUUUUU");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Gnam Gnam sto mangiando l'erba");
+        }
+    }
+
+    public class Tigre : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Roaaaaaaar");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Gnam Gnam sto mangiando la carne");
+        }
+    }
+
+    public class Cane : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Bau Bau");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Gnam Gnam sto mangiando i croccantini(?)");
+        }
+    }
+
+    public class Balena : Animale, INuotante
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("WOOOOOOUUUUOOO (Non so come scrivere l'onomatopea di una balena)");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Gnam Gnam sto mangiando il Plancton");
+        }
+
+        public void Nuota()
+        {
+            Console.WriteLine("SI NUOTAAAAAAAAAAAAAA");
+        }
+    }
+
+    public class Piccione : Animale, IVolante
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Prrrr Prrrrr");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Gnam Gnam sto mangiando il miglio(?)");
+        }
+
+        public void Vola()
+        {
+            Console.WriteLine("SI VOLAAAAAAAAAAAAAA");
+        }
+    }
+
+    public interface IVolante
+    {
+        public void Vola();
+    }
+
+    public interface INuotante {
+        public void Nuota();
+    }
+}
--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace csharp_abstract_animals
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace csharp_abstract_animals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Animale> animali = new List<Animale>();
+            animali.Add(new Cane());
+            animali.Add(new Mucca());
+            animali.Add(new Tigre());
+            animali.Add(new Balena());
+            animali.Add(new Piccione());
+
+            foreach (Animale animale in animali)
+            {
+                Console.WriteLine(animale.ToString().Substring(24));
+                animale.Verso();
+                animale.Mangia();
+                Console.WriteLine("\n");
+            }
+
+            void FaiVolare(IVolante v)
+            {
+                v.Vola();
+            }
+
+            void FaiNuotare(INuotante n)
+            {
+                n.Nuota();
+            }
+
+            Piccione piccione = new Piccione();
+            Console.Write($"\nPiccione dice: ");
+            FaiVolare(piccione);
+
+            Balena balena = new Balena();
+            Console.Write($"\nBalena dice: ");
+            FaiNuotare(balena);
         }
     }
 }
